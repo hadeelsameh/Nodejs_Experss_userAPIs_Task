c59cc5910af5f0e1c730c8ed410ee06bcb567d81
hadeelsameh <hadeelsameh24@gmail.com>
2022-08-30 07:44:43 +0200

    commit

---
+++ server.js
@@ -0,0 +1,35 @@
+let express = require('express');
+let readline = require("readline");
+let fs = require('fs').promises;
+let app = express()
+
+
+app.get('/getusername', function (req, res) {
+
+    let name = req.query.nn;
+    let age= req.query.gg;
+    
+    fs.appendFile("text.txt",`${name} `).then(function(){});
+    res.json({
+        name: req.query.nn,
+    });
+
+})
+
+app.get('/listusers', function (req, res) {
+
+    let users=[] ;
+    fs.readFile("text.txt").then(function(data){
+        data = data.toString();
+        users = data.split(" ");
+        users.pop();
+        res.json({
+            users
+        });
+    });
+
+    
+})
+
+
+app.listen(3000)
